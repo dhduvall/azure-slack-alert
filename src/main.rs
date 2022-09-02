86d17f9a1f876f9ef5039f83285a9743454a945a
Danek Duvall <duvall@comfychair.org>
2022-09-02 14:49:47 -0700

    add tracing and a dummy connection to the keyvault

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,22 @@ use axum::{
     routing::get,
     Router,
 };
+use azure_identity::{AzureCliCredential, DefaultAzureCredential, DefaultAzureCredentialEnum};
+use azure_security_keyvault::SecretClient;
 use std::collections::HashMap;
 use std::env;
 use std::net::{Ipv4Addr, SocketAddr};
+use tracing::{instrument, trace};
+use tracing_subscriber;
+use uname::uname;
 
 pub mod alerts;
 
 #[tokio::main]
+#[instrument]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/api/ServiceHealthAlert", get(do_get).post(do_post));
 
     let port_envvar = "FUNCTIONS_CUSTOMHANDLER_PORT";
@@ -30,6 +38,7 @@ async fn main() {
         .unwrap();
 }
 
+#[instrument]
 async fn do_get(params: Option<Query<HashMap<String, String>>>) -> String {
     let Query(params) = params.unwrap_or_default();
     match params.get("name") {
@@ -40,6 +49,7 @@ async fn do_get(params: Option<Query<HashMap<String, String>>>) -> String {
 
 // All the return types must be the same.  If that's not appropriate, we can call .into_response():
 // https://docs.rs/axum/0.5.15/axum/response/index.html#returning-different-response-types
+#[instrument(skip(v))]
 async fn do_post(v: Result<Json<alerts::ActivityLog>, JsonRejection>) -> impl IntoResponse {
     match v {
         Ok(v) => match &v.data.context.activity_log {
@@ -88,10 +98,65 @@ async fn do_post(v: Result<Json<alerts::ActivityLog>, JsonRejection>) -> impl In
     }
 }
 
-async fn handle_service_health(_ev: &alerts::ServiceHealth) -> impl IntoResponse {
-    (StatusCode::OK, String::from("Got Service Health event"))
+#[instrument(skip(_ev))]
+async fn handle_service_health(
+    _ev: &alerts::ServiceHealth,
+) -> axum::response::Result<(StatusCode, String)> {
+    // On MacOS, a connection to 169.254.169.254 (which happens for the managed identity
+    // credential) doesn't always return.  A simple curl to that will hang, too, the first time,
+    // and then every connection after that will give "Host is down" until it seems to reset
+    // itself.  169.254/16 is used for host-to-host connections when there's no real network.  All
+    // references to this I can find are about being unable to connect to a real network, not this
+    // problem.  So we'll have to work around it.
+    let creds = if uname().unwrap().sysname == "Darwin" {
+        trace!("Darwin system; using Azure CLI credential");
+        DefaultAzureCredential::with_sources(vec![DefaultAzureCredentialEnum::AzureCli(
+            AzureCliCredential {},
+        )])
+    } else {
+        trace!("Using default Azure credential options");
+        DefaultAzureCredential::default()
+    };
+
+    // XXX vault name from env
+    let client = SecretClient::new(
+        "https://coros-svc-health-alert.vault.azure.net",
+        std::sync::Arc::new(creds),
+    )
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            String::from(format!("Failed to initialize Key Vault client: {e}")),
+        )
+    })?;
+    trace!("Created client; retrieving secret");
+
+    // XXX Get this from the environment or something.
+    let secret_name = "test-secret";
+    // It would be nice to make this map_err() call more compact, maybe by having a map_ise()
+    // function.  But the map_err() argument is a function that takes a single Error argument, and
+    // we'd need to be able to pass the string, too.  Can we do something like have map_ise()
+    // return a function that does the right thing, and then put map_ise(msg) as the argument to
+    // map_err()?
+    let secret = client.get(secret_name).into_future().await.map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            String::from(format!(
+                "Failed to retrieve secret '{secret_name}' from Key Vault: {e}"
+            )),
+        )
+    })?;
+
+    Ok((
+        StatusCode::OK,
+        String::from(format!(
+            "Got Service Health event, secret '{secret_name}' is '{}'",
+            secret.value,
+        )),
+    ))
 }
 
+#[instrument(skip(_ev))]
 async fn handle_security_log(_ev: &alerts::SecurityLog) -> impl IntoResponse {
     (
         StatusCode::BAD_REQUEST,
@@ -99,6 +164,7 @@ async fn handle_security_log(_ev: &alerts::SecurityLog) -> impl IntoResponse {
     )
 }
 
+#[instrument(skip(_ev))]
 async fn handle_recommendation(_ev: &alerts::Recommendation) -> impl IntoResponse {
     (
         StatusCode::BAD_REQUEST,
@@ -106,6 +172,7 @@ async fn handle_recommendation(_ev: &alerts::Recommendation) -> impl IntoRespons
     )
 }
 
+#[instrument(skip(_ev))]
 async fn handle_resource_health(_ev: &alerts::ResourceHealth) -> impl IntoResponse {
     (
         StatusCode::BAD_REQUEST,
@@ -113,6 +180,7 @@ async fn handle_resource_health(_ev: &alerts::ResourceHealth) -> impl IntoRespon
     )
 }
 
+#[instrument(skip(_ev))]
 async fn handle_administrative(_ev: &alerts::Administrative) -> impl IntoResponse {
     (
         StatusCode::BAD_REQUEST,
